98ac4997f62da74f114dae945fb0ad747efe2793
AKASH MURIKI <85022436+akash-muriki@users.noreply.github.com>
2022-09-26 14:58:24 +0530

    Add files via upload

---
+++ scripts/background.js
@@ -0,0 +1,14 @@
+console.log("background running");
+
+// chrome.action.onClicked.addListener(buttonClicked);
+ 
+
+
+// function buttonClicked() {
+//     console.log("Button clicked");
+// }
+
+
+// $("run-script-btn").click(function(){
+//     console.log("Done!");
+// })
