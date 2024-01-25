fad6f9c356c42d22e61ee941214868dd54425330
rohith500 <rohithreddy2202@gmail.com>
2024-01-26 01:49:51 +0530

    Add rating system

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,11 +1,27 @@
 $(document).ready(function () {
+  let clickCount = 0;
+  let displayCount = 0;
+
+
   $("#run-script-btn").click(function () {
     // storing data in local storage
     let text = $("#content").val();
 
-    chrome.storage.local.set({ lastCopied: text }, function () {});
+    chrome.storage.local.set({ lastCopied: text }, function () { });
     copyURL();
+
+    // Check if the user should see the card
+    if (clickCount % 5 === 0) {
+      displayCount++;
+
+      // Show card as a popup every 20 times
+      if (displayCount % 10 === 0) {
+        showCardPopup();
+      }
+    }
   });
+
+
 
   $(".links").click(function () {
     var link = $(this).attr("name");
@@ -22,6 +38,17 @@ $(document).ready(function () {
       $(".b3").css("opacity", 0);
     }
   );
+  $("#google-icon").hover(
+    function () {
+      $(".b3").css("opacity", 1);
+    },
+    function () {
+      $(".b3").css("opacity", 0);
+    }
+  );
+  $("#close-card").click(function () {
+    clickCount++;
+  });
 });
 const copyURL = () => {
   const copyDiv = document.querySelector(".copyAlert:not(.animate)");
@@ -32,3 +59,27 @@ const copyURL = () => {
     );
   }
 };
+
+function showCardPopup() {
+  // Check if the user has clicked on the card before
+  const clicked = localStorage.getItem("clicked");
+
+  if (clicked !== "true") {
+    // Show the card modal
+    $('#cardModal').modal('show');
+
+    $("#dont-ask").on("click", () => {
+      $('#cardModal').modal('hide');
+      localStorage.setItem("clicked", "true");
+    });
+
+    // Add event listener for closing the card
+    $("#close-card").on("click", () => {
+      // Hide the modal
+      $('#cardModal').modal('hide');
+
+      // Update localStorage to indicate that the user has clicked on the card
+      localStorage.setItem("clicked", "true");
+    });
+  }
+}
